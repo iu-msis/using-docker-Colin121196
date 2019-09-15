bb1b81cbac363f981ce5727b79e21d22a541e138
Colin121196 <coliooi@iu.edu>
2019-09-15 19:45:24 -0400

    Update reload button to reload component

--- public/js/ProfileInformation.js
+++ public/js/ProfileInformation.js
@@ -1,16 +1,20 @@
 var usersApp = new Vue({
   el: '#userProfileApp',
   data: {
-    users: []
+    users: [],
+    componentKey: 0,
   },
   methods: {
-    fetchUsers() {
-      fetch('https://randomuser.me/api/')
-      .then(response => response.json())
-      .then(json => {usersApp.users = json});
-    }
+  fetchUsers() {
+    fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(json => {usersApp.users = json});
+  }
   },
   created() {
     this.fetchUsers()
+  },
+  forceRerender() {
+    this.componentKey += 1;
   }
 });
