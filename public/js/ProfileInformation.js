a93d9feaef626ca3ddd52365e082789e4b0fa4e9
Colin121196 <coliooi@iu.edu>
2019-09-15 18:42:51 -0400

    Updated RandomUser.me API to work in UserInformation page

--- public/js/ProfileInformation.js
+++ public/js/ProfileInformation.js
@@ -1,22 +1,16 @@
 var usersApp = new Vue({
   el: '#userProfileApp',
   data: {
-    patients: []
+    users: []
   },
   methods: {
-    fetchPatients() {
+    fetchUsers() {
       fetch('https://randomuser.me/api/')
       .then(response => response.json())
-      .then(json => {usersApp.patients = json});
-
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
-
+      .then(json => {usersApp.users = json});
     }
   },
   created() {
-    this.fetchPatients()
+    this.fetchUsers()
   }
 });
